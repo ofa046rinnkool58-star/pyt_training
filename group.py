f5696859299cbadc70bb310b5f5d180ddd261b51
MIN <ofa046rinnkool58@gmail.com>
2026-07-23 19:01:53 +0300

    Второе задание (тест через рекордер + структурирование)

---
+++ group.py
@@ -0,0 +1,5 @@
+class Group:
+    def __init__(self, groupName, headerName, footerName):
+        self.groupName = groupName
+        self.headerName = headerName
+        self.footerName = footerName
