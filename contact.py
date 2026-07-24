a182c510f49af4e7c52f05e48b8e3d31388efd28
MIN <ofa046rinnkool58@gmail.com>
2026-07-24 18:09:14 +0300

    Третье задание (аналогичное второму + контакты)

---
+++ contact.py
@@ -0,0 +1,15 @@
+class Contact:
+    def __init__(self, firstName, middleName, lastName, nickName, Adress, Phone1, Phone2, Phone3, Mail1, Mail2, Mail3):
+        self.firstname = firstName
+        self.middlename = middleName
+        self.lastname = lastName
+        self.nickname = nickName
+        self.adress = Adress
+        self.phone1 = Phone1
+        self.phone2 = Phone2
+        self.phone3 = Phone3
+        self.mail1 = Mail1
+        self.mail2 = Mail2
+        self.mail3 = Mail3
+
+
